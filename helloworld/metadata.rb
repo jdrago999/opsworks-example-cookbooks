b84c20ce2427a500049832614896392519de0eed
John Drago <drago@inflection.com>
2014-11-19 11:45:18 -0800

    Trying it with the 'deploy' dependency

--- helloworld/metadata.rb
+++ helloworld/metadata.rb
@@ -7,6 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 
-cookbook 'deploy', git: 'https://github.com/aws/opsworks-cookbooks'
-depends "deploy"
 
