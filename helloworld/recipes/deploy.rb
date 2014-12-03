0e30ea74f0ef1ee3ba154c24bd5219513aebd353
John Drago <jdrago.999@gmail.com>
2014-12-03 14:51:31 -0800

    Update deploy.rb

--- helloworld/recipes/deploy.rb
+++ helloworld/recipes/deploy.rb
@@ -1,7 +1,7 @@
 
 node[:deploy].each do |app_name, deploy|
   bash 'deploy-stuff' do
-    code 'curl https://news.ycombinator.com/ > /tmp/hn.html'
+    code "curl https://news.ycombinator.com/ > #{deploy[:deploy_to]}/public/hn.html"
   end
 
 
